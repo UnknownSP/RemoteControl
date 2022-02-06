bbcda79991592db2bb198207f3b0b805ad12a624
UnknownSP <monsterhunter.sp.games@gmail.com>
2022-02-07 07:08:09 +0900

    シリアル通信失敗時の対応を追加

--- remotecontrol_bot.js
+++ remotecontrol_bot.js
@@ -23,21 +23,79 @@ token = token_file.toString().split('\r\n')[3];//個人鯖
 COM_RECEIVE_CHANNEL = token_file.toString().split('\r\n')[4];
 COM_RECEIVE_CHANNEL_ID = token_file.toString().split('\r\n')[5];
 
-const port = new SerialPort('COM4', { baudRate: 115200 });
-const parser = port.pipe(new Readline({ delimiter: '\n' }));
-
 var _display_info = false;
 var _display = false;
 
-var max_setCount = 50;
+var max_setCount = 10;
 var setCount = 0;
 var setTimerObj = new Array(max_setCount).fill(null);
 
-port.on('open', function () {
-    console.log('Serial open.');
-    setInterval(writeToArduino, 10000, 'GET_INFO\n', "not_Timer");
+var max_sendCount = 10;
+var sendCount = 0;
+var sendcheckCount = 0;
+var sendcheckCommand = new Array(max_sendCount).fill(null);
+var _sendcheck_flag = new Array(max_sendCount).fill(false);
+
+var opts = {
+    width: 1920,
+    height: 1080,
+    quality: 80,
+    frames: 60,
+    delay: 0,
+    saveShots: true,
+    output: "jpeg",
+    device: false,
+    callbackReturn: "location",
+    verbose: false
+};
+var Webcam = NodeWebcam.create( opts );
+
+var intervalObj;
+
+const port = new SerialPort('COM4', { baudRate: 115200 }, function(err){
+    if(err){
+        console.log('Com Port Error.');
+        setTimeout(sendToDiscord, 2000, "[ErrorCode : 01]\nArduinoとの通信に失敗しました。5秒後に自動復帰を行います。");
+        setTimeout(portOpen,5000);
+    }
 });
 
+port.on('open', function (err) {
+    if(err){
+        console.log('Com Port Error.');
+        setTimeout(sendToDiscord, 2000, "[ErrorCode : 01]\nArduinoとの通信に失敗しました。5秒後に自動復帰を行います。");
+        setTimeout(portOpen,5000);
+        return;
+    }
+    console.log('Serial open.');
+    setTimeout(sendToDiscord, 2000, "Arduinoとの通信を開始しました。");
+    intervalObj = setInterval(writeToArduino, 600000, 'GET_INFO\n', "not_Timer");
+});
+
+function portOpen(){
+    port.open(function (err) {
+        if(err){
+            console.log(err);
+            setTimeout(sendToDiscord, 2000, "[ErrorCode : 03]\nArduinoとの通信復帰に失敗しました。");
+        }
+        setTimeout(sendToDiscord, 2000, "Arduinoとの通信復帰成功。");
+    });
+}
+function resetPort(){
+    try {
+        clearInterval(intervalObj);
+        port.close(function (err) {
+            if(err){
+                console.log(err);
+            }
+        });
+        setTimeout(portOpen,5000);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
 parser.on('data', data =>{
     console.log('from arduino:', data);
     var send_mess = "";
@@ -64,26 +122,57 @@ parser.on('data', data =>{
         }
         send_mess += " (RawData : " + Bright + ")\n";
         _display = false;
+    }else if(data.indexOf("Received : ") != -1){
+        if(data.indexOf(sendcheckCommand[sendcheckCount])){
+            _sendcheck_flag[sendcheckCount] = false;
+            sendcheckCount++;
+            if(sendcheckCount == max_sendCount) sendcheckCount = 0;
+        }
     }
     if(_display_info){
         if(send_mess != ""){
-            client.channels.cache.get(COM_RECEIVE_CHANNEL_ID).send(send_mess);
+            sendToDiscord(send_mess);
         }
         if(!_display){
-            client.channels.cache.get(COM_RECEIVE_CHANNEL_ID).send("-------------------------------------");
+            sendToDiscord("-------------------------------------");
             _display_info = false;
         }
     }
 });
 
+function sendToDiscord(mess){
+    client.channels.cache.get(COM_RECEIVE_CHANNEL_ID).send(mess);
+}
+
+function sendCheck(check_number){
+    if(_sendcheck_flag[check_number]){
+        console.log('Could not Send');
+        sendToDiscord("[ErrorCode : 02]\nArduinoとの通信に失敗しました。5秒後に自動復帰を行います。");
+        resetPort();
+        sendcheckCount = 0;
+        sendCount = 0;
+        for(var i=0; i<max_sendCount; i++){
+            sendcheckCommand[i] = null;
+            _sendcheck_flag[i] = false;
+        }
+        return;
+    }
+    console.log("Complete");
+}
+
 function writeToArduino(data, deleteTimer_command) {
     console.log('Write: ' + data);
     port.write(data, function(err, results) {
-      if(err) {
-        console.log('Err: ' + err);
-        console.log('Results: ' + results);
-      }
+        if(err) {
+            console.log('Err: ' + err);
+            console.log('Results: ' + results);
+        }
     });
+    sendcheckCommand[sendCount] = data;
+    _sendcheck_flag[sendCount] = true;
+    setTimeout(sendCheck, 2000, sendCount);
+    sendCount++;
+    if(sendCount == max_sendCount) sendCount = 0;
 
     if(deleteTimer_command != "not_Timer"){
         deleteTimer_Com(deleteTimer_command);
@@ -91,7 +180,7 @@ function writeToArduino(data, deleteTimer_command) {
 }
 function clearTimer_Num(deleteTimer_number){
     if(setTimerObj[deleteTimer_number] == null){
-        client.channels.cache.get(COM_RECEIVE_CHANNEL_ID).send("指定された予約は存在しません");
+        sendToDiscord("[ErrorCode : 40]\n指定された予約は存在しません");
         return;
     }
     clearTimeout(setTimerObj[deleteTimer_number]);
@@ -118,7 +207,7 @@ function clearTimer_Num(deleteTimer_number){
         console.log(error);
         return;
     }
-    client.channels.cache.get(COM_RECEIVE_CHANNEL_ID).send("予約を消去しました");
+    sendToDiscord("予約を消去しました");
     sendSetTimer();
     console.log('予約消去完了');
     setCount -= 1;
@@ -149,9 +238,9 @@ function deleteTimer_Com(deleteTimer_command){
         console.log(error);
         return;
     }
-    client.channels.cache.get(COM_RECEIVE_CHANNEL_ID).send("予約 " + deleteTimer_command+" を実行しました");
-    sendSetTimer();
-    console.log('予約消去完了');
+    sendToDiscord("予約 " + deleteTimer_command+" のコマンド送信完了");
+    setTimeout(sendSetTimer,1000);
+    console.log('予約実行完了');
     setCount -= 1;
 }
 
@@ -164,7 +253,8 @@ function sendSetTimer(){
     }
     var setTimer_data = setTimer_file.toString().split('\n');
     if(setTimer_data.length == 1){
-        client.channels.cache.get(COM_RECEIVE_CHANNEL_ID).send("現在予約はありません");
+        sendToDiscord("現在予約はありません");
+        sendToDiscord("-------------------------------------");
         return;
     }
     var send_mess = "現在の予約は以下のようになっています\n";
@@ -172,28 +262,46 @@ function sendSetTimer(){
         send_mess += "[" +i+"]\n" + new Date(Number(setTimer_data[i].split(',')[1])) +"\n"+ setTimer_data[i].split(' ')[1] +" "+ setTimer_data[i].split(' ')[2] + "\n";
     }
     send_mess += "-------------------------------------";
-    client.channels.cache.get(COM_RECEIVE_CHANNEL_ID).send(send_mess);
+    sendToDiscord(send_mess);
 }
-
-var opts = {
-    width: 1920,
-    height: 1080,
-    quality: 80,
-    frames: 60,
-    delay: 0,
-    saveShots: true,
-    output: "jpeg",
-    device: false,
-    callbackReturn: "location",
-    verbose: false
-};
-var Webcam = NodeWebcam.create( opts );
-//Webcam.capture( "test_picture", function( err, data ) {} );
 
 client.login(token);
 
 client.on("ready", ()=> {
     console.log("起動完了....");
+    try {
+        var setTimer_file = fs.readFileSync("setTimer.txt", 'utf8');
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+    var setTimer_data = setTimer_file.toString().split('\n');
+    if(setTimer_data.length == 1){
+        return;
+    }
+    var writeLine = "";
+    var writeCount = 0;
+    for(var i=0; i<setTimer_data.length - 1;i++){
+        var timeDiff = Number(setTimer_data[i].split(',')[1]) - Date.now();
+        if(timeDiff > 0){
+            setTimerObj[setCount] = setTimeout(writeToArduino, timeDiff, setTimer_data[i].split(',')[3], setTimer_data[i].split(',')[2]);
+            writeLine += writeCount + ","+ setTimer_data[i].split(',')[1] +","+ setTimer_data[i].split(',')[2] +","+ setTimer_data[i].split(',')[3] + "\n";
+            writeCount ++;
+            _set_timer = false;
+            setCount++; 
+        }
+    }
+    try {
+        fs.writeFileSync("setTimer.txt", writeLine);
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+    if(writeCount == 0){
+        return;
+    }
+    sendToDiscord("起動前に予約されていた予約をセットしました\n");
+    sendSetTimer();
 });
 
 client.on("message",async message => {
@@ -208,11 +316,17 @@ client.on("message",async message => {
     var _set_timer = false;
     var set_time = 0;
 
-    if(!message.author.bot){
-        console.log("-------------------------------------");
-        console.log(receivemessage);
-        console.log(message.author.username+" (id : "+message.id+")");
+    if(argument_3 == undefined){
+        argument_3 = "";
     }
+
+    if(message.author.bot){
+        return;
+    }
+    
+    console.log("-------------------------------------");
+    console.log(receivemessage);
+    console.log(message.author.username+" (id : "+message.id+")");
 
     switch(receivecommand){
         case COMMAND_PREFIX+"ip":
@@ -221,13 +335,16 @@ client.on("message",async message => {
                 .then(res => res.text())
                 .then((text) => {
                     console.log(text);
-                    client.channels.cache.get(COM_RECEIVE_CHANNEL_ID).send("現在のIPは\n\n"+ text +"\n(誰にもみせないでね)");
+                    sendToDiscord("現在のIPは\n\n"+ text);
                 })
                 .catch(err => console.log(err));
             break;
+        case COMMAND_PREFIX+"reset":
+            resetPort();
+            break;
         case COMMAND_PREFIX+"get":
             if(argument_2 == null){
-                message.reply("Usage:  /get [Command]");
+                sendToDiscord("[ErrorCode : 10]\nUsage:  /get [Command]");
                 break;
             }
             switch(argument_2){
@@ -238,34 +355,39 @@ client.on("message",async message => {
                     break;
                 case "Photo":
                     Webcam.capture( "picture", async function( err, data ) {
+                        if(err){
+                            console.log("WebCam Error");
+                            sendToDiscord("[ErrorCode : 30]\nWebカメラが接続されていません");
+                            return;
+                        }
                         const canvas = Canvas.createCanvas(1920, 1080);
                         const ctx = canvas.getContext('2d');
                         const background = await Canvas.loadImage('./picture.jpg');
                         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
                         const attachment = new MessageAttachment(canvas.toBuffer(), 'picture.jpg');
-                        client.channels.cache.get(COM_RECEIVE_CHANNEL_ID).send(attachment);
+                        sendToDiscord(attachment);
                     } );
                     break;
                 case "Timer":
                     sendSetTimer();
                     break;
                 default:
-                    message.reply("そのようなコマンドはありません");
+                    sendToDiscord("[ErrorCode : 11]\nそのようなコマンドはありません");
                     break;
             }
             break;
         case COMMAND_PREFIX+"clear":
             if(argument_2 == null){
-                message.reply("Usage:  /clear [Timer Number]");
+                sendToDiscord("[ErrorCode : 10]\nUsage:  /clear [Timer Number]");
                 break;
             }
             if(setCount == 0){
-                message.reply("現在予約はありません");
+                sendToDiscord("現在予約はありません");
                 break;
             }
             var Timer_num = Number(argument_2);
             if(Timer_num < 0 || Timer_num > (setCount-1)){
-                message.reply("そのような予約はありません");
+                sendToDiscord("[ErrorCode : 41]\nそのような予約はありません");
                 break;
             }
             clearTimer_Num(Timer_num);
@@ -274,12 +396,12 @@ client.on("message",async message => {
         case COMMAND_PREFIX+"send":
             if(receivecommand == (COMMAND_PREFIX+"set")){
                 if(argument_2 == null || argument_3 == null || argument_4 == null){
-                    message.reply("Usage:  /set [Target] [Command] [Time(m)]");
+                    sendToDiscord("[ErrorCode : 10]\nUsage:  /set [Target] [Command] [Time(m)]");
                     break;
                 }
                 if(argument_2 == "Webcam" && argument_3 == "Target"){
                     if(argument_5 == null){
-                        message.reply("Usage:  /set Webcam Target [degree] [Time(m)]");
+                        sendToDiscord("[ErrorCode : 10]\nUsage:  /set Webcam Target [degree] [Time(m)]");
                         break;
                     }else{
                         set_time = Number(argument_5);
@@ -289,14 +411,14 @@ client.on("message",async message => {
                 }
                 if(set_time > 0 && set_time <= 600){
                     _set_timer = true;
-                    set_time = set_time /* *60 */*1000;
+                    set_time = set_time *60 *1000;
                 }else{
-                    message.reply("Timerの指定時間は0～600分以内です");
+                    sendToDiscord("[ErrorCode : 50]\nTimerの指定時間は1～600分以内です");
                     break;
                 }
             }else if(receivecommand == (COMMAND_PREFIX+"send")){
                 if(argument_2 == null || argument_3 == null){
-                    message.reply("Usage:  /send [Target] [Command]");
+                    sendToDiscord("[ErrorCode : 10]\nUsage:  /send [Target] [Command]");
                     break;
                 }   
             }
@@ -307,7 +429,7 @@ client.on("message",async message => {
                             send_command = "TV_POWER\n";
                             break;
                         default:
-                            message.reply("そのようなコマンドはありません");
+                            sendToDiscord("[ErrorCode : 11]\nそのようなコマンドはありません");
                             break;
                     }
                     break;
@@ -320,7 +442,7 @@ client.on("message",async message => {
                             send_command = "LIGHT_OFF\n";
                             break;
                         default:
-                            message.reply("そのようなコマンドはありません");
+                            sendToDiscord("[ErrorCode : 11]\nそのようなコマンドはありません");
                             break;
                     }
                     break;
@@ -339,7 +461,7 @@ client.on("message",async message => {
                             send_command = "AC_DEHUMID\n";
                             break;
                         default:
-                            message.reply("そのようなコマンドはありません");
+                            sendToDiscord("[ErrorCode : 11]\nそのようなコマンドはありません");
                             break;
                     }
                     break;
@@ -364,7 +486,7 @@ client.on("message",async message => {
                             send_command = "DS_AIR_MINUS\n";
                             break;
                         default:
-                            message.reply("そのようなコマンドはありません");
+                            sendToDiscord("[ErrorCode : 11]\nそのようなコマンドはありません");
                             break;
                     }
                     break;
@@ -378,32 +500,37 @@ client.on("message",async message => {
                             break;
                         case "Target":
                             if(argument_4 == null){
-                                message.reply("Usage:  /send Webcam Target [degree]");
+                                sendToDiscord("[ErrorCode : 10]\nUsage:  /send Webcam Target [degree]");
                                 break;
                             }
                             var degree =  Number(argument_4);
                             if(degree >= 0 && degree <= 180){
                                 send_command = "SERVO_TARGET_" + degree + "\n";
                             }else{
-                                message.reply("WebCamの指定角度は0～180度以内です");
+                                sendToDiscord("[ErrorCode : 60]\nWebCamの指定角度は0～180度以内です");
                             }
                             break;
                         default:
-                            message.reply("そのようなコマンドはありません");
+                            sendToDiscord("[ErrorCode : 11]\nそのようなコマンドはありません");
                             break;
                     }
                     break;
                 default:
-                    message.reply("そのようなデバイスはありません");
+                    sendToDiscord("[ErrorCode : 20]\nそのようなデバイスはありません");
                     break;
             }
             break;
         default:
+            sendToDiscord("[ErrorCode : 11]\nそのようなコマンドはありません");
             break;
     }
 
     if(send_command != ""){
         if(_set_timer){
+            if(setCount >= max_setCount){
+                sendToDiscord("[ErrorCode : 42]\n予約数が"+max_setCount+"に達しています。これ以上予約できません");
+                return;
+            }
             setTimerObj[setCount] = setTimeout(writeToArduino, set_time, send_command, message.content.replace(/　/g," "));
             var setDate = Date.now()+Number(set_time);
             try {
@@ -412,16 +539,16 @@ client.on("message",async message => {
                 console.log(error);
                 return;
             }
-            message.reply("予約完了\n" + new Date(setDate)+ ", "+ argument_2 + ", "+ argument_3);
+            sendToDiscord("予約完了\n" + new Date(setDate)+ ", "+ argument_2 + ", "+ argument_3);
             _set_timer = false;
             setCount++;
         }else{
             writeToArduino(send_command, "not_Timer");
+            sendToDiscord("コマンド送信完了\n" +argument_2 + ", "+ argument_3);
         }
     }
 
-    if(!message.author.bot){
-        console.log("-------------------------------------");
-        client.channels.cache.get(COM_RECEIVE_CHANNEL_ID).send("-------------------------------------");
-    }
+    console.log("-------------------------------------");
+    sendToDiscord("-------------------------------------");
+    
 });
